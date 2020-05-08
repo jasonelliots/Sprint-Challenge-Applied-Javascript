93fe11e563df9095d8c288ec94520dfae318dc11
jasonelliots <j.elliotschwartz@gmail.com>
2020-05-08 08:59:21 -0700

    completed cards

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -19,12 +19,163 @@
 //
 // Use your function to create a card for each of the articles and add the card to the DOM.
 
+const cardsContainer = document.querySelector('.cards-container')
+
 function cardCreator(){
 
-    const cardDiv = document.createElement("div")
+    axios.get('https://lambda-times-backend.herokuapp.com/articles')
+    
+    .then(response => {
+        console.log(response)
+        response.data.articles.javascript.forEach(article => {
 
+            const cardDiv = document.createElement("div")
+            const headlineDiv = document.createElement("div")
+            const authorDiv = document.createElement("div")
+            const imgDiv = document.createElement("div")
+            const authorImg = document.createElement("img")
+            const authorName = document.createElement("span")
+        
+            cardDiv.appendChild(headlineDiv)
+            cardDiv.appendChild(authorDiv)
+            authorDiv.appendChild(imgDiv)
+            authorDiv.appendChild(authorName)
+            imgDiv.appendChild(authorImg)
+        
+            cardDiv.classList.add('card')
+            headlineDiv.classList.add('headline')
+            authorDiv.classList.add('author')
+            imgDiv.classList.add('img-container')
+
+            headlineDiv.textContent = article.headline
+            authorImg.src = article.authorPhoto
+            authorName.textContent = article.authorName
+
+            // console.log(cardDiv)
+
+            cardsContainer.appendChild(cardDiv)
+        })
+
+        response.data.articles.node.forEach(article => {
+
+            const cardDiv = document.createElement("div")
+            const headlineDiv = document.createElement("div")
+            const authorDiv = document.createElement("div")
+            const imgDiv = document.createElement("div")
+            const authorImg = document.createElement("img")
+            const authorName = document.createElement("span")
+        
+            cardDiv.appendChild(headlineDiv)
+            cardDiv.appendChild(authorDiv)
+            authorDiv.appendChild(imgDiv)
+            authorDiv.appendChild(authorName)
+            imgDiv.appendChild(authorImg)
+        
+            cardDiv.classList.add('card')
+            headlineDiv.classList.add('headline')
+            authorDiv.classList.add('author')
+            imgDiv.classList.add('img-container')
+
+            headlineDiv.textContent = article.headline
+            authorImg.src = article.authorPhoto
+            authorName.textContent = article.authorName
+
+            // console.log(cardDiv)
+
+            cardsContainer.appendChild(cardDiv)
+
+        })
+
+        response.data.articles.jquery.forEach(article => {
+
+            const cardDiv = document.createElement("div")
+            const headlineDiv = document.createElement("div")
+            const authorDiv = document.createElement("div")
+            const imgDiv = document.createElement("div")
+            const authorImg = document.createElement("img")
+            const authorName = document.createElement("span")
+        
+            cardDiv.appendChild(headlineDiv)
+            cardDiv.appendChild(authorDiv)
+            authorDiv.appendChild(imgDiv)
+            authorDiv.appendChild(authorName)
+            imgDiv.appendChild(authorImg)
+        
+            cardDiv.classList.add('card')
+            headlineDiv.classList.add('headline')
+            authorDiv.classList.add('author')
+            imgDiv.classList.add('img-container')
+
+            headlineDiv.textContent = article.headline
+            authorImg.src = article.authorPhoto
+            authorName.textContent = article.authorName
+
+            // console.log(cardDiv)
+
+            cardsContainer.appendChild(cardDiv)
+
+        })
+        response.data.articles.bootstrap.forEach(article => {
+
+            const cardDiv = document.createElement("div")
+            const headlineDiv = document.createElement("div")
+            const authorDiv = document.createElement("div")
+            const imgDiv = document.createElement("div")
+            const authorImg = document.createElement("img")
+            const authorName = document.createElement("span")
+        
+            cardDiv.appendChild(headlineDiv)
+            cardDiv.appendChild(authorDiv)
+            authorDiv.appendChild(imgDiv)
+            authorDiv.appendChild(authorName)
+            imgDiv.appendChild(authorImg)
+        
+            cardDiv.classList.add('card')
+            headlineDiv.classList.add('headline')
+            authorDiv.classList.add('author')
+            imgDiv.classList.add('img-container')
+
+            headlineDiv.textContent = article.headline
+            authorImg.src = article.authorPhoto
+            authorName.textContent = article.authorName
+
+            // console.log(cardDiv)
+
+            cardsContainer.appendChild(cardDiv)
+
+        })
+
+        response.data.articles.technology.forEach(article => {
+
+            const cardDiv = document.createElement("div")
+            const headlineDiv = document.createElement("div")
+            const authorDiv = document.createElement("div")
+            const imgDiv = document.createElement("div")
+            const authorImg = document.createElement("img")
+            const authorName = document.createElement("span")
+        
+            cardDiv.appendChild(headlineDiv)
+            cardDiv.appendChild(authorDiv)
+            authorDiv.appendChild(imgDiv)
+            authorDiv.appendChild(authorName)
+            imgDiv.appendChild(authorImg)
+        
+            cardDiv.classList.add('card')
+            headlineDiv.classList.add('headline')
+            authorDiv.classList.add('author')
+            imgDiv.classList.add('img-container')
+
+            headlineDiv.textContent = article.headline
+            authorImg.src = article.authorPhoto
+            authorName.textContent = article.authorName
+
+            // console.log(cardDiv)
+
+            cardsContainer.appendChild(cardDiv)
+
+        })
+    })
 
 }
 
-
-cardCreator 
+cardCreator() 
